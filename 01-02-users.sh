7e9287991cdbb51de83f51723f50aeb8d9f72a0e
maxpeigne <peigneanthony@hotmail.fr>
2025-09-28 17:31:39 +0200

    01-02 - Reste la ligne 41 pour le pas afficher le / apres le 'fich11'

--- 01-02-users.sh
+++ 01-02-users.sh
@@ -8,7 +8,8 @@ do
 	$us
 done
 
-echo "Affectation des groupes"
+echo " ----------------------------------------"
+echo -e "\e[33mAffectation des groupes\e[0m"
 sudo usermod -G group1 user1
 
 sudo usermod -G group1 user2
@@ -17,41 +18,37 @@ sudo usermod -G group2 user4
 sudo usermod -aG group1 user4
 sudo usermod -aG group2 user2
 
-echo "Creation de repertoires et fichiers"
+echo " ----------------------------------------"
+echo -e "\e[36mCreation de repertoires et fichiers\e[0m"
 sudo mkdir rep1 rep2 rep3
 sudo touch rep1/fich11
 sudo mkdir rep2/rep21
 
-echo "Manipulation des fichiers"
+echo " ----------------------------------------"
+echo -e "\e[35mManipulation des fichiers\e[0m"
 cd rep2/rep21/
 sudo cp -r ../../rep1/ .
 cd ../..
 sudo cp -r rep2/ rep3/
 
-echo "Visualisation détaillée de rep3"
-array=()
-index=1
-path="rep3/"
-echo -n "Chemin $index : $path"
-ls $path
-echo "resultat du ls = $?"
-path+=$(ls $path)
-echo "resultat du ls = $?"
-path+="/"
-echo "path suivant = $path"
-path+=$(ls $path)
-echo "resultat du ls = $?"
-path+="/"
-echo "3e path = $path"
-path+=$(ls $path)
-path+="/"
-echo "resultat du ls = $?"
-echo "path suivant = $path"
-path+=$(ls $path)
-echo "resultat du ls = $?"
-echo "path suivant = $path"
-path+=$(ls $path)
-echo "resultat du ls = $?"
-path+=$(ls $path)
-echo "resultat du ls = $?"
-echo "path suivant = $path"
+echo " ----------------------------------------"
+#Visualisation détaillée de rep3
+path="rep3"
+ls $path > /dev/null
+resultLs=$?
+while (( $resultLs == 0 ))
+do
+	path+="/"
+	path+=$(ls $path 2>/dev/null)
+	resultLs=$?
+done
+echo "Le chemin detaille de rep3 est: $path"
+
+echo " ----------------------------------------"
+echo -e "\e[31m Suppresion de rep3\e[0m"
+sudo rm -r rep3/
+ls
+
+echo " ----------------------------------------"
+echo -e "\e[44mVoici les 5 dernieres lignes de passwd:\e[0m"
+tail -n 5 /etc/passwd
